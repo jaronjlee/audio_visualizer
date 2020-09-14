639660daff864d9545f4b232e238705dc3bc779c
jaronlee <jaronjlee@gmail.com>
2020-09-14 15:30:46 -0700

    css

--- dist/style.97fcb138.js
+++ dist/style.97fcb138.js
@@ -194,7 +194,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"/node_modules/@fortawesome/fontawesome-free/css/all.css":"node_modules/@fortawesome/fontawesome-free/css/all.css","/Users/jaronlee/Desktop/audio_visualizer 5/beach.jpg":[["/dist/beach.0b90b284.jpg","beach.jpg"],"beach.jpg"],"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"/node_modules/@fortawesome/fontawesome-free/css/all.css":"node_modules/@fortawesome/fontawesome-free/css/all.css","/Users/jaronlee/Desktop/audio_visualizer 5/beach.jpg":[["/beach.0b90b284.jpg","beach.jpg"],"beach.jpg"],"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
